0285333c14e50cc72d7421c687d04ed0cfe46e11
Howard Stark <howard@getcoffee.io>
2017-10-10 13:04:24 -0700

    Handler func

--- server.go
+++ server.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-	"net"
-
 	"net/http"
+
+	"bytes"
+	"io"
+	"strings"
+
+	"io/ioutil"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -19,15 +23,36 @@ func NewServer() *Server {
 }
 
 func (s *Server) init() {
-	listener, listenerErr := net.Listen("tcp", flagServerPort)
-	if listenerErr != nil {
-		log.Panic(listenerErr)
-	}
-	for {
-		conn, connErr := listener.Accept()
-		if connErr != nil {
-			log.Error("Error accepting incoming request")
-		}
+	http.HandleFunc("/upload", HandlerUpload)
+	log.Fatal(http.ListenAndServe(":22101", nil))
+}
 
+func HandlerUpload(w http.ResponseWriter, r *http.Request) {
+	var localBuff bytes.Buffer
+
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		r.Body.Close()
+		log.WithError(err).Error("Could not handle file upload")
+		return
 	}
+	defer file.Close()
+	name := strings.Split(header.Filename, ".")
+	log.WithFields(log.Fields{
+		"filename": header.Filename,
+	}).Info("New strategy uploaded")
+	io.Copy(&localBuff, file)
+
+	writeErr := ioutil.WriteFile(name[0]+".ipd", localBuff.Bytes(), 0755)
+	if writeErr != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		r.Body.Close()
+		log.WithError(err).Error("Issue opening file for writing")
+		return
+	}
+
+	localBuff.Reset()
+	w.WriteHeader(http.StatusOK)
+	r.Body.Close()
 }
