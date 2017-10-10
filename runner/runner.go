6c54b1ba0c95d86194e0b1d4c221b2968cbaa234
Leijurv <leijurv@gmail.com>
2017-10-10 12:29:35 -0700

    invalid examples

--- runner/runner.go
+++ runner/runner.go
@@ -11,13 +11,21 @@ import (
 )
 
 func main() {
-	contenders := []string{"../examples/random.py", "../examples/titfortat.py", "../examples/allcoop.py", "../examples/alldefect.py"}
+	contenders := []string{"../examples/random.py", "../examples/titfortat.py", "../examples/allcoop.py", "../examples/alldefect.py", "../examples/invalidcommand.py", "../examples/invalidquit.py"}
 	for i := 0; i < len(contenders); i++ {
 		for j := 0; j < len(contenders); j++ {
 			if i == j {
 				continue
 			}
-			a, b, rounds := playAgainst(contenders[i], contenders[j])
+			a, b, rounds, adisqual, bdisqual := playAgainst(contenders[i], contenders[j])
+			if adisqual{
+				fmt.Println("WARNING contender A: ",contenders[i]," IS DISQUALIFIED")
+				continue
+			}
+			if bdisqual{
+				fmt.Println("WARNING contender B: ",contenders[j]," IS DISQUALIFIED")
+				continue
+			}
 			fmt.Println(contenders[i], "vs", contenders[j])
 			fmt.Println(contenders[i], "avg:", a)
 			fmt.Println(contenders[j], "avg:", b)
@@ -28,22 +36,24 @@ func main() {
 
 }
 
+var imagID string
+func init(){
+	imagIDRaw, err := exec.Command("docker", "build", "-q", "dock").CombinedOutput()
+	if err != nil {
+		fmt.Println(string(imagIDRaw))
+		panic(err)
+	}
+	imagID = strings.Trim(string(imagIDRaw), "\n")
+}
 //takes in two executable paths
 //starts up two of these dockers, with port 10000 in the VM bound to two random numbers on the host
 //copies in the two executables to /code in both images
 //makes a connection to localhost:whatever, which is docker:10000 on both. this makes relay.go start the provided /code, and forwards stdin/stdout over this socket
 //runs iterated prisoner's dilemma, and finally returns number of rounds and final socre
 
-func playAgainst(pathToExecA string, pathToExecB string) (float32, float32, int) {
-
-	imagIDRaw, err := exec.Command("docker", "build", "-q", "dock").CombinedOutput()
-	if err != nil {
-		fmt.Println(string(imagIDRaw))
-		panic(err)
-	}
-	imagID := strings.Trim(string(imagIDRaw), "\n")
+func playAgainst(pathToExecA string, pathToExecB string) (AavgScore float32, BavgScore float32, numRounds int,aDisqualified bool,bDisqualified bool) {
 	//fmt.Println("Image id:",imagID)
-
+	time.Sleep(500*time.Millisecond)
 	containerARaw, err := exec.Command("docker", "run", "--rm", "-d", "-p", "5000:10000", imagID).CombinedOutput()
 	if err != nil {
 		fmt.Println(string(containerARaw))
@@ -94,27 +104,30 @@ func playAgainst(pathToExecA string, pathToExecB string) (float32, float32, int)
 	BScore := 0
 
 	numTurns := 0
-
+	A.SetDeadline(time.Now().Add(2 * time.Second)) // generous deadline for first move
+	B.SetDeadline(time.Now().Add(2 * time.Second))
 	for {
-		A.SetDeadline(time.Now().Add(50 * time.Millisecond))
-		B.SetDeadline(time.Now().Add(50 * time.Millisecond))
 		Am := make([]byte, 1)
 		_, err = io.ReadFull(A, Am)
 		if err != nil {
-			panic(err) // todo dont panic, just disqualify
+			fmt.Println("Disqualified due to stream closed")
+			return 0,0,0,true,false
 		}
 		Bm := make([]byte, 1)
 		_, err = io.ReadFull(B, Bm)
 		if err != nil {
-			panic(err) // todo dont panic, just disqualify
+			fmt.Println("Disqualified due to stream closed")
+			return 0,0,0,false,true
 		}
 
 		if Am[0] != 0 && Am[0] != 1 {
-			panic("A made invalid move") // todo dont panic, just disqualify
+			fmt.Println("A made invalid move")
+			return 0,0,0,true,false
 		}
 
 		if Bm[0] != 0 && Bm[0] != 1 {
-			panic("B made invalid move") // todo dont panic, just disqualify
+			fmt.Println("B made invalid move")
+			return 0,0,0,false,true
 		}
 
 		Amove := Am[0] == 1
@@ -138,9 +151,11 @@ func playAgainst(pathToExecA string, pathToExecB string) (float32, float32, int)
 		if rnd.Float32() < 0.0003 && numTurns > 100 {
 			break
 		}
+		A.SetDeadline(time.Now().Add(50 * time.Millisecond)) // short deadline for subsequent moves
+		B.SetDeadline(time.Now().Add(50 * time.Millisecond))
 	}
 
-	return float32(AScore) / float32(numTurns), float32(BScore) / float32(numTurns), numTurns
+	return float32(AScore) / float32(numTurns), float32(BScore) / float32(numTurns), numTurns,false,false
 }
 
 const R = 3
