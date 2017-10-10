6094fbb2a2782dab3d96740bf3cb622f637f6c5b
Leijurv <leijurv@gmail.com>
2017-10-10 09:58:14 -0700

    woohoo

--- runner/dock/relay.go
+++ runner/dock/relay.go
@@ -4,6 +4,8 @@ import(
 	"io"
 	"net"
 	"os/exec"
+	"os/signal"
+	"os"
 )
 
 
@@ -17,16 +19,61 @@ func main(){
 	if err != nil {
 		panic(err)
 	}
+	defer conn.Close()
 	fmt.Println("Relay has received its connection") // one time use
 
-	cmd := exec.Command("/code")
-	in, _ := cmd.StdinPipe()
-    out, _ := cmd.StdoutPipe()
+	os.Chmod("/code",0777)
 
-    go io.Copy(in,conn)
-    go io.Copy(conn,out)
+
+	signalChan := make(chan os.Signal, 1)
+
+	timeToExit := make(chan bool)
+
+	signal.Notify(signalChan, os.Interrupt)
+
+	go func() {
+	    for _ = range signalChan {
+		        fmt.Println("\nReceived an interrupt, stopping services...\n")
+	        timeToExit <- true
+	    }
+	}()
+
+	cmd := exec.Command("/code")
+
+	cmd.Stderr = os.Stdout
+
+	in, err := cmd.StdinPipe()
+	if err != nil {
+		panic(err)
+	}
+	defer in.Close()
+
+    out, err := cmd.StdoutPipe()
+    if err != nil {
+		panic(err)
+	}
+	defer out.Close()
+
+    go func(){
+    	io.Copy(in,conn)
+    	fmt.Println("Done copying from command to socket")
+    	timeToExit <- true
+    }()
+
+    go func(){
+    	io.Copy(conn,out)
+    	fmt.Println("Done copying from socket to command")
+    	timeToExit <- true
+    }()
 
     cmd.Start()
-    cmd.Wait()
+    go func(){
+    	 cmd.Wait()
+    	 fmt.Println("Command done waiting")
+    	 timeToExit <- true
+    }()
+   
+   	<- timeToExit
+   	fmt.Println("Exiting")
 
 }
