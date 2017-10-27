bc9f1513950b73993d60732c15076d934dabbf1b
Izzy Swart <syntacticsugarglider@gmail.com>
2017-10-27 11:29:20 -0700

    minor text change

--- app/src/components/About.vue
+++ app/src/components/About.vue
@@ -3,7 +3,7 @@
     <div class="content">
       <h1>About</h1>
       <div class="writing">
-        <p><strong>Bastille</strong> is an Iterated Prisoner's Dilemma (IPD) tournament manager designed to remove the limitations that afflict similar solutions. Bastille is language agnostic, which is achieved by <strong>containerizing the execution of uploaded strategies</strong>. Strategies interact with each other through an unshared supervisor which sends opponent move information via <i>stdin</i> and reads your program's moves via <i>stdout</i>.</p>
+        <p><strong>Bastille</strong> is an Iterated Prisoner's Dilemma (IPD) tournament manager designed to remove the limitations that afflict similar solutions. Bastille is language agnostic, which is achieved by <strong>containerizing the execution of uploaded strategies</strong>. Strategies interact with each other through an unshared supervisor which sends opponent move information via standard in and reads your program's moves via standard out.</p>
         <p>The simple nature of the interaction with the strategies allows for <strong>tens of thousands of rounds to happen in just a few seconds</strong>. Additionally, to ensure that the scale of the games isn't compromised by overly complicated strategies, there is a timeout on each move, after which your strategy will disqualified.</p>
         <p>This was made by Mickey, Leif, and Izzy. If you would like to become a contributor, feel free to email me (howard@getcoffee.io). If you want to check out the source, visit <a href="https://github.com/howardstark/bastille">https://github.com/howardstark/bastille</a>.</p>
       </div>
